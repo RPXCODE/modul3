7ddfb0e4a7476a42966d5680cc95dd8140f9789c
iqodhul himam <iqodhulhimam2@gmail.com>
2025-10-24 16:38:59 +0700

    update 4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class KalkulatorInteraktif {
+public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double angka1, angka2, hasil;
